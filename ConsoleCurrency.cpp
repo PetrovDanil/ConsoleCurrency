37dac450db983e126a3827773ff49c21950e303a
kapacnk <petrowdanil349@yandex.ru>
2025-03-17 17:20:22 +0300

    added source files

---
+++ ConsoleCurrency.cpp
@@ -0,0 +1,12 @@
+﻿// ConsoleCurrency.cpp: определяет точку входа для приложения.
+//
+
+#include "ConsoleCurrency.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
